a2ae2be1e0eb40e5fa21968c66283e6a67169d29
Dhruv <dhruvdp.2004@gmail.com>
2025-09-13 15:22:04 -0700

    added frame and video

---
+++ frame/frame.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import sys
+import cv2
+
+import os
+def capture_frame(output_file: str):
+    cap = cv2.VideoCapture(0, cv2.CAP_V4L2)  # /dev/video0
+    if not cap.isOpened():
+        print("❌ Could not open /dev/video0")
+        sys.exit(1)
+
+    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+
+    ok, frame = cap.read()
+    cap.release()
+
+    if not ok:
+        print("failed to capture frame")
+        sys.exit(1)
+
+    cv2.imwrite(output_file, frame) # imwrite method can handle most image formats.
+    print(f"Saved frame to {output_file}")
+
+if __name__ == "__main__":
+    output = sys.argv[1] if len(sys.argv) > 1 else "frame.png"
+    if "." not in output.lower():
+        output += ".png"
+
+    os.makedirs("pics", exist_ok=True)
+    output = "pics/" + output
+    
+    capture_frame(output)
---
+++ video/video.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import cv2
+import sys
+
+def open_camera():
+    cap = cv2.VideoCapture(0, cv2.CAP_V4L2)  # /dev/video0
+    if not cap.isOpened():
+        print("❌ Could not open /dev/video0")
+        sys.exit(1)
+
+    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+    return cap
+
+
+def main():
+    cap = open_camera()
+
+    print("press q to quit the video window.")
+    while True:
+        ok, frame = cap.read()
+        if not ok:
+            print("failed to grab frame")
+            break
+
+        cv2.imshow("Live Feed", frame)
+
+        # press q to quit
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
